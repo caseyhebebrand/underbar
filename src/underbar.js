df46a94bbb6d004566b2770ae594ebc897cfa759
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-07-25 21:11:45 -0700

    "Some"

--- src/underbar.js
+++ src/underbar.js
@@ -212,6 +212,9 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return !(_.every(collection, function(item) {
+      return iterator ? !iterator(item) : !item;
+    }));
   };
 
 
